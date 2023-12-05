9a3a10f0c38501a977be2b1bb4b53edeb22ef2b4
Feda Almodhi <143366228+floatingsanity@users.noreply.github.com>
2023-12-04 23:36:49 -0500

    update2

--- src/mychip-polaris.js
+++ src/mychip-polaris.js
@@ -2,14 +2,17 @@ import { LitElement, html, css } from 'lit';
 
 class MychipPolaris extends LitElement {
   static properties = {
-    header: { type: String },
+    link: { type: String },
+    name: { type: String },
   }
 
   static styles = css`
     :host {
       display: block;
     }
+
     .tag-module--tag--d9ee2 {
+      flex: 1;
       text-decoration: none;
       color: #001e44;
       border: 2px solid #444;
@@ -20,49 +23,29 @@ class MychipPolaris extends LitElement {
       letter-spacing: 0.03em;
       line-height: 19px;
       text-align: center;
-      padding: 13px;
+      padding: 17px;
       margin: 5px;
+      width: 85%;
       transition: background-color 0.3s, color 0.3s, text-decoration 0.3s, border 0.3s;
       -webkit-text-size-adjust: 100%;
       -webkit-font-smoothing: antialiased;
       box-sizing: inherit;
       text-transform: uppercase;
     }
+
     .tag-module--tag--d9ee2:hover {
       background-color: #f0f0f0;
       color: #0052be;
       text-decoration: underline;
       border: 2px solid transparent;
     }
-
-    .tag-container {
-      display: grid;
-      grid-template-columns: repeat(3, 1fr);
-      gap: 10px;
-      margin-left: 70px
-    }
   `;
 
   render() {
     return html`
-      <span>${this.header}</span>
-      <div class="tag-container">
-        <a class="tag-module--tag--d9ee2" href="https://www.psu.edu/news/academics/">Academics</a>
-        <a class="tag-module--tag--d9ee2" href="https://www.psu.edu/news/students/">Students</a>
-        <a class="tag-module--tag--d9ee2" href="https://www.psu.edu/news/visitors-and-neighbors/">Visitors and Neighbors</a>
-        <a class="tag-module--tag--d9ee2" href="https://www.psu.edu/news/faculty-and-staff/">Faculty and Staff</a>
-        <a class="tag-module--tag--d9ee2" href="https://www.psu.edu/news/business-and-industry/">Business and Industry</a>
-        <a class="tag-module--tag--d9ee2" href="https://www.psu.edu/news/alumni/">Alumni</a>
-        <a class="tag-module--tag--d9ee2" href="https://www.psu.edu/news/university-park/">University Park</a>
-        <a class="tag-module--tag--d9ee2" href="https://www.psu.edu/news/latest-news/">Latest News</a>
-        <a class="tag-module--tag--d9ee2" href="https://www.psu.edu/news/academics/rankings/">Rankings</a>
-        <a class="tag-module--tag--d9ee2" href="https://www.psu.edu/news/impact/">Impact</a>
-        <a class="tag-module--tag--d9ee2" href="https://www.psu.edu/news/impact/global/">Global</a>
-        <a class="tag-module--tag--d9ee2" href="https://www.psu.edu/news/global-programs/">Global Programs</a>
-      </div>
+      <a class="tag-module--tag--d9ee2" href="${this.link}">${this.name}</a>
     `;
   }
 }
 
 customElements.define('mychip-polaris', MychipPolaris);
-
